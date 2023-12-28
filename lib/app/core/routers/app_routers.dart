b8273f3d8e872884a5d5cc5537c92c0ecbd90173
Jonathan Willian <jonathan1313>
2023-12-28 11:25:20 -0300

    feat: create middleware to redirect user after see onboarding page

--- lib/app/core/routers/app_routers.dart
+++ lib/app/core/routers/app_routers.dart
@@ -4,11 +4,16 @@ import '../../features/habits/presentation/pages/home/home_page.dart';
 import '../../features/onboarding/presentation/pages/onboarding_page.dart';
 import '../../features/authentication/presentation/pages/authentication_page.dart';
 
+import 'middlewares/onboarding_view_middleware.dart';
+
 class AppRouters {
   static final pages = [
     GetPage(
       name: '/onboarding',
       page: () => OnboardingPage(),
+      middlewares: [
+        OnboardingViewMiddleware(),
+      ]
     ),
     GetPage(
       name: '/auth',
