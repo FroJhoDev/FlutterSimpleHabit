6db4c400e4029e480a1ebaf9e63ef5d9d29150af
Jonathan Willian <jonathan1313>
2023-12-28 19:31:09 -0300

    chore: remove supabase from project, change for firebase to better compatibility with architecture

--- lib/app/core/utils/app_constants.dart
+++
@@ -1,5 +0,0 @@
-
-abstract class AppConstants {
-  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL', defaultValue: '');
-  static const String supabaseAnnonKey = String.fromEnvironment('SUPABASE_ANNON_KEY', defaultValue: '');
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-import 'app/core/utils/app_constants.dart';
 
 import 'app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: AppConstants.supabaseUrl, anonKey: AppConstants.supabaseAnnonKey);
   await GetStorage.init();
   runApp(const MyApp());
 }
