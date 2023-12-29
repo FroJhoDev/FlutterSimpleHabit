83d537a4f5479119613e954e4c8f1c421bf11077
Jonathan Willian <jonathan1313>
2023-12-29 09:14:52 -0300

    feat: link firebase project to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:simple_habit/firebase_options.dart';
 
 import 'app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
